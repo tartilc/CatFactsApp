1f7233f0b994b71273a45ddd6c6fe39c447106e7
NickShute <nshute@umich.edu>
2022-04-21 14:02:57 -0400

    Moved recycle view into a bottom fragment
    Also added the test plan to the repo

---
+++ app/src/main/java/com/example/catfactsapp/BottomViewModel.java
@@ -0,0 +1,7 @@
+package com.example.catfactsapp;
+
+import androidx.lifecycle.ViewModel;
+
+public class BottomViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/catfactsapp/bottomFragment.java
@@ -0,0 +1,36 @@
+package com.example.catfactsapp;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class bottomFragment extends Fragment {
+
+    private BottomViewModel mViewModel;
+
+    public static bottomFragment newInstance() {
+        return new bottomFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.bottom_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(BottomViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
